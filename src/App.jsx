1955b6451428b984d2de7d2c3b852cd35764180d
Nic0Su4 <nicolas.suarez.vera@gmail.com>
2023-04-23 23:11:01 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react'
+import styled from '@emotion/styled'
+import Formulario from './components/Formulario'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+
+function App() {
+
+  const [monedas, setMonedas] = useState({})
+  const [resultado, setResultado] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(monedas).length > 0) {
+      const { moneda, cripto } = monedas;
+      const cotizarCripto = async () => {
+        setIsLoading(true);
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        setResultado(resultado.DISPLAY[cripto][moneda]);
+
+        setIsLoading(false);
+      }
+      cotizarCripto();
+    }
+  }, [monedas]);
+
+  return (
+    <>
+      <h1 style={{ textAlign: 'center', color: 'white', fontFamily: 'Lato' }}>Convertidor de Criptos Culo Tímido, cuidao Torres</h1>
+      <Contenedor>
+        <Imagen
+          src={ImagenCripto}
+          alt='Imagen criptomonedas'
+        />
+        <div>
+          <Heading>Cotiza <span style={{ color: 'red' }}>Criptomonedas</span> al <span style={{ color: 'red' }}>instante</span></Heading>
+
+          <Formulario
+            setMonedas={setMonedas}
+          />
+
+          {isLoading ? <Spinner />
+            :
+            <Resultado resultado={resultado} />}
+        </div>
+      </Contenedor>
+    </>
+  )
+}
+
+export default App
---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+  {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+  {id: 'PEN', nombre: 'Sol Peruano'},
+  {id: 'EUR', nombre: 'Euro'},
+  {id: 'MXN', nombre: 'Peso Mexicano'},
+]
+
+export {monedas}
